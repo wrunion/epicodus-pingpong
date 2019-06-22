514cccdd4fd5ec1566e6575ae07ef4e69c519109
Winter Runion <winterrunion@gmail.com>
2019-06-21 19:28:54 -0700

    Fix JS indentation.

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,11 @@ $(document).ready(function() {
     for (i = 1; i <= number; i++) {
       if (i % 15 === 0) {
       $("#result").append("<li>"+ "pingpong" + "</li>");
-    } else
-      if (i % 3 === 0) {
-        $("#result").append("<li>"+ "ping" + "</li>"); } else
-      if (i % 5 === 0) {
-        $("#result").append("<li>"+ "pong" + "</li>"); }
-      else
+      } else if (i % 3 === 0) {
+        $("#result").append("<li>"+ "ping" + "</li>");
+      } else if (i % 5 === 0) {
+        $("#result").append("<li>"+ "pong" + "</li>");
+      } else
        $("#result").append("<li>"+ i + "</li>"); }
     event.preventDefault();
   });
