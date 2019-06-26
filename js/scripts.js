6cc700f2bde8b1f42e1ed98e0c04b710dbe76999
Winter Runion <winterrunion@gmail.com>
2019-06-26 16:31:16 -0700

    Create empty results array and push values into it upon form submit.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,32 @@
+var resultArray = [];
+
+// function empty() {
+//   resultArray.clear();
+// }
+
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
+
     var number = parseInt($("#number").val());
-    // alert(number);
+
     var i;
     for (i = 1; i <= number; i++) {
       if (i % 15 === 0) {
-      $("#result").append("<li>"+ "pingpong" + "</li>");
+        resultArray.push("pingpong");
       } else if (i % 3 === 0) {
-        $("#result").append("<li>"+ "ping" + "</li>");
+        resultArray.push("ping");
       } else if (i % 5 === 0) {
-        $("#result").append("<li>"+ "pong" + "</li>");
+        resultArray.push("pong");
       } else
-       $("#result").append("<li>"+ i + "</li>"); }
+       resultArray.push(i); }
+
+      resultArray.forEach(function(element) {
+        $("#result ul").append("<li>" + element + "</li>");
+      });
     event.preventDefault();
+    // $("form").trigger('reset');
+  });
+  $("button#reset").click(function(event) {
+   location.reload();
   });
 });
